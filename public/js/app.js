9c26fe2430e7db3a9bf6c5c2d5378c531966cb70
Adriana Vazquez <luna.vazquezv@gmail.com>
2019-08-27 01:05:08 -0500

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,6 @@ var limit = 140;
 var counter = 0;
 var counterSpan = document.getElementById('counter');
 var text = document.getElementById('text').addEventListener('input', function(event) {
-    if(!counter) return false;
     var stringSize = event.target.value.length;
     counter = limit - stringSize;
     counterSpan.innerHTML = counter;
